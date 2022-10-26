2c4e0b9d33e9fa948acb24a883ab4f28de9f563a
yufeng <321353225@qq.com>
2022-10-26 16:31:17 +0800

    just use bare

--- cortex-quickstart/src/main.rs
+++ cortex-quickstart/src/main.rs
@@ -10,7 +10,7 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m::asm;
 use cortex_m_rt::entry;
 // use cortex_m_semihosting::{debug, hprintln};
-use cortex_m_semihosting::debug;
+// use cortex_m_semihosting::debug;
 
 #[entry]
 fn main() -> ! {
@@ -18,7 +18,7 @@ fn main() -> ! {
 
     // hprintln!("Hello World!").unwrap();
 
-    debug::exit(debug::EXIT_SUCCESS);
+    // debug::exit(debug::EXIT_SUCCESS);
 
     loop {
         // your code goes here
